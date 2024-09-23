899c693de248adca4297dbaed019cd3d8e4549af
Thickant <141985730+Thickant@users.noreply.github.com>
2024-09-23 18:15:43 -0400

    Update dataSkills.js

--- scripts/data/dataSkills.js
+++ scripts/data/dataSkills.js
@@ -17,7 +17,7 @@ define([''], function () {
     icon: 34,
     stats: [['bsh', 50]],
     multis: [['arm', 1.10]],
-    tooltip: '40% chance to perform\nSHIELD Strike:\nAdds 30% of DEF to ATK\nwhen calculating damage.\nGives 10% more DEF'
+    tooltip: '50% chance to perform\nSHIELD Strike:\nAdds 30% of DEF to ATK\nwhen calculating damage.\nGives 10% more DEF'
   }, {
     icon: 35,
     stats: [['iml', 5]],
