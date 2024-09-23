3a19920bdfccb11096ea2acf3262ba80ba30bc9e
Thickant <141985730+Thickant@users.noreply.github.com>
2024-09-23 14:13:03 -0400

    Update character.js

--- scripts/character.js
+++ scripts/character.js
@@ -156,7 +156,7 @@ define(['game', 'sat', 'stats', 'entity', 'dataSkills', 'dataRelics', 'dataActio
         var _this4 = this;
 
         if (this.iml) {
-          var distance = 50;
+          var distance = 50 + (this.iml * 5); // Burn distance increases by 5 for each immolation level
           var nearby = game.entities.aliveEnemies().filter(function (e) {
             return Math.abs(e.x - _this4.x) <= distance && Math.abs(e.y - _this4.y) <= distance;
           });
