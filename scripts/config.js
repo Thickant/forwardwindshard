47074f1f0395a7e77473d3f8641d328766454572
Thickant <141985730+Thickant@users.noreply.github.com>
2024-09-22 20:45:09 -0400

    Add files via upload

---
+++ scripts/config.js
@@ -0,0 +1,26 @@
+"use strict";
+
+define([''], function () {
+  //*******************************************************************************************************************
+  // ** The Game Configuration
+  //*******************************************************************************************************************
+  var config = {};
+  config.startWith = 'title'; //Options: title, quickload, newgame
+
+  config.showTips = true;
+  config.cheats = false;
+  config.loadSfx = true;
+  config.loadOst = true;
+  config.sfxDefaultVolume = 0.8;
+  config.ostDefaultVolume = 0.5;
+  config.api = 'None';
+  config.logo = [];//['agintro', 'https://armor.ag/MoreGames'];
+
+  config.armorMode = false; //true
+
+  config.domainLock = ''; //'armorgames.com'
+
+  config.showAdventaleLogo = true;
+  config.adventaleWebsite = '';
+  return config;
+});
