f2785d2b12c49d3580d5a136eee78904e4d940d3
Thickant <141985730+Thickant@users.noreply.github.com>
2024-10-07 11:33:17 -0400

    Update character.js

--- scripts/character.js
+++ scripts/character.js
@@ -64,6 +64,7 @@ define(['game', 'sat', 'stats', 'entity', 'dataSkills', 'dataRelics', 'dataActio
       };
       _this.level = 1;
       _this.experience = 0;
+      _this.psn = 1;
       _this.items = [];
       _this.equips = [];
       _this.attributes = [];
