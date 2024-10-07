922615b076528125c038223d212c8e3f41ee041d
Thickant <141985730+Thickant@users.noreply.github.com>
2024-10-07 12:03:17 -0400

    Update dataSkills.js

--- scripts/data/dataSkills.js
+++ scripts/data/dataSkills.js
@@ -11,7 +11,7 @@ define([''], function () {
     tooltip: '25% more HP. but 5% less DEF'
   }, {
     icon: 33,
-    stats: [['lch', 4], ['psn', 200]],
+    stats: [['lch', 4], ['psn', 10]],
     tooltip: '4% LEECH. but gain 2% POISON'
   }, {
     icon: 34,
