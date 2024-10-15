b11acabf6305cd8c8fcdb14ce743f855b65a9112
Thickant <141985730+Thickant@users.noreply.github.com>
2024-10-15 17:48:59 -0400

    Update dataSkills.js

--- scripts/data/dataSkills.js
+++ scripts/data/dataSkills.js
@@ -110,6 +110,58 @@ define([''], function () {
     icon: 55,
     stats: [['gbs', 150]],
     tooltip: 'LEECH and REGEN are\nincreased by 50% while under\nGUARD Spell.'
+  }, // Page 3
+  {
+    icon: 176,
+    stats: [['frz', 1], ['arr', -2], ['fbm', 2.5]],
+    tooltip: '-2 ARROWS.\nGrants an additional FREEZE Spell.\nFrozen enemies take 2.5x\ndamage from BURN.'
+  }, {
+    icon: 177,
+    stats: [['lch', 10], ['psn', 5], ['slm', -30]],
+    tooltip: '10% LEECH, 5% POISON.\nCRITICAL and SHIELD Strikes\nleech 30% less.'
+  }, {
+    icon: 46,
+    stats: [['arr', -1], ['bsh', 50], ['slm', 10]],
+    tooltip: '-1 ARROW.\nAdditional 50% chance to\nperform SHIELD Strike.\nCRITICAL and SHIELD Strikes\n leech 10% more'
+  }, {
+    icon: 44,
+    stats: [['abs', 12]],
+    tooltip: 'Take 12% less damage.'
+  }, {
+    icon: 48,
+    stats: [['arr', 1]],
+    multis: [['dmg', 0.95]],
+    tooltip: '1 ARROW.\n 5% less ATK'
+  }, {
+    icon: 49,
+    stats: [['are', 1], ['hnt', 1]],
+    tooltip: 'All spent ARROWS are recovered\nwhen you use GUARD Spell.\nDamage of ARROWS is calculated\non DEF instead of ATK, but at\n30% of the value.'
+  }, {
+    icon: 50,
+    stats: [['arf', 20]],
+    tooltip: '20% of HP is added to\nATK when calculating\nARROW damage.'
+  }, {
+    icon: 51,
+    stats: [['crm', 50]],
+    multis: [['dmg', 0.80]],
+    tooltip: '20% less ATK.\nCRITICAL Strikes deal 200%\ninstead of 150% damage.'
+  }, {
+    icon: 52,
+    stats: [['iml', -5]],
+    multis: [['dmg', 1.05], ['arm', 1.05]],
+    tooltip: '-5% BURN.\n5% more ATK.\n5% more DEF.'
+  }, {
+    icon: 53,
+    multis: [['mhp', 1.10], ['dmg', 1.04], ['arm', 1.08]],
+    tooltip: '10% more HP.\n4% more ATK.\n8% more DEF.'
+  }, {
+    icon: 54,
+    stats: [['rag', 0.5]],
+    tooltip: 'Gain 0.5% more ATK\nwhen you are hit.\nStacks additively.'
+  }, {
+    icon: 55,
+    stats: [['gbs', 150]],
+    tooltip: 'LEECH and REGEN are\nincreased by 50% while under\nGUARD Spell.'
   }];
   return skills;
 });
